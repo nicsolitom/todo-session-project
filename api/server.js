a259c820101f7cb43ec39a49499c995b6c617fd0
Nic Solitom <nicsolitom@gmail.com>
2020-02-19 16:14:30 +0100

    Connected to MongoDB Atlas

--- api/server.js
+++ api/server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const api = express();
 const session = require('express-session');
 const mongoose = require('mongoose');
+
+const multiUserTodoDB =
+  'mongodb+srv://dbMultipleUserToDo:solitom@multiusertodoapplication-ehiv6.gcp.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose
+  .connect(multiUserTodoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDb Atlas connected'));
