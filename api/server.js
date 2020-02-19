b442eac805abc16b8e17f31888529e5349219fdb
Nic Solitom <nicsolitom@gmail.com>
2020-02-19 16:41:06 +0100

    Added: Allow cross origin access from frontend - in server.js

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const UsersList = require('./userModel');
 const TodoList = require('./todoModel');
 
+//? Connecting with MongoDB Atlas:
 const multiUserTodoDB =
   'mongodb+srv://dbMultipleUserToDo:solitom@multiusertodoapplication-ehiv6.gcp.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -15,3 +16,18 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => console.log('MongoDb Atlas connected'));
+
+//? ALLOW CROSS ORIGIN ACCESS FROM FRONTEND
+api.use((req, res, next) => {
+  res.set('ACCESS-CONTROL-ALLOW-ORIGIN', 'http://localhost:5000');
+  res.set('ACCESS-CONTROL-ALLOW-CREDENTIALS', 'true');
+  res.set(
+    'ACCESS-CONTROL-ALLOW-HEADERS',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
+  res.set(
+    'ACCESS-CONTROL-ALLOW-METHODS',
+    'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS'
+  );
+  next();
+});
